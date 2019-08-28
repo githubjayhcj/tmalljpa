8d9d840b85eb669239051632a758813418f6f81b
githubjayhcj <1178376362@qq.com>
2019-08-28 13:28:21 +0800

    8-28 -latest

--- src/test/java/com/taobao/tmailjpa/TmalljpaApplicationTests.java
+++ src/test/java/com/taobao/tmailjpa/TmalljpaApplicationTests.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = TmalljpaApplicationTests.class)
 public class TmalljpaApplicationTests {
 
     @Test
     public void contextLoads() {
+        System.out.println("test");
+
     }
 
 }
